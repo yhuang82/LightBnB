70e531d9833677dbded3bdaa98ffa0830c1b4fda
Yiqun Huang <albert913388672@gmail.com>
2023-10-19 22:25:12 -0700

    implement the getAllReservations

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -78,7 +78,22 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(
+      `SELECT *
+      FROM reservations
+      WHERE guest_id = $1
+      ORDER BY start_date
+      LIMIT $2;`,
+      [guest_id, limit]
+    )
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /// Properties
